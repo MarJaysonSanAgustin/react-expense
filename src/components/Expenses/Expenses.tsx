b26e68b2afb5df574224f90c00cac3397705427e
Mar Jayson San Agustin <sanagustinjayson@gmail.com>
2022-06-18 17:47:45 +0800

    refactor: add expense filter handler

--- src/components/Expenses/Expenses.tsx
+++ src/components/Expenses/Expenses.tsx
@@ -1,15 +1,25 @@
 import ExpenseItem, { ExpenseItemProps } from "./ExpenseItem";
+import ExpensesFilter from "./ExpenseFilter";
 import Card from "../UI/Card";
 import './Expenses.css';
+import { useState } from "react";
 
 export interface ExpensesProps {
   expenses: ExpenseItemProps[];
+  onYearFilterChange: Function;
 }
-const Expenses = ({ expenses }: ExpensesProps) => {
+const Expenses = ({ expenses, onYearFilterChange }: ExpensesProps) => {
+  const [yearFilter, setYearFilter] = useState('2022');
+
+  const onFilterChange = (yearFilter: string) => {
+    onYearFilterChange(yearFilter);
+    setYearFilter(yearFilter);
+  }
   return (
     <Card>
+      <ExpensesFilter selectedFilter={yearFilter} onFilterChange={onFilterChange} />
       <div className="expenses">
-        {expenses?.map(expense => <ExpenseItem {...expense} />)}
+        {expenses?.length < 1 ? <p className="expenses__empty-message">No expenses found.</p> : expenses?.map(expense => <ExpenseItem key={expense.id} {...expense} />)}
       </div>
     </Card>
   )
